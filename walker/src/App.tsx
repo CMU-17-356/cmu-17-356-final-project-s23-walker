54b129f75fdb0db4ee82662b7e60bd25288d649e
Philip Kaufholz <pkaufhol@andrew.cmu.edu>
2023-04-11 12:29:57 -0400

    install CRA frontend and express  backend with ESLint

---
+++ walker/src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
---
+++ walker/src/Home.tsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            Walker
+          </header>
+        </div>
+      );
+}
+
+export default Home
