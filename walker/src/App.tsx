2fa036aa27e434dbf0f4249f520e0982c8148f46
Michelle <mliuu3259@gmail.com>
2023-04-11 14:40:36 -0400

    login page

--- walker/src/App.tsx
+++ walker/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
+import Login from "./pages/Login";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Landing />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </BrowserRouter>
     );
