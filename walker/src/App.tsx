c94b11e9b6d2681bff9d7699f05e2ab79fbc158e
Michelle <mliuu3259@gmail.com>
2023-04-11 14:27:52 -0400

    made landing page the root

--- walker/src/App.tsx
+++ walker/src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Home";
 import Landing from "./pages/Landing";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/landing" element={<Landing />} />
+                <Route path="/" element={<Landing />} />
             </Routes>
         </BrowserRouter>
     );
