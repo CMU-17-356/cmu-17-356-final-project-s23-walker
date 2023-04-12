046d2affdfadb25afe9697c2312fb11b6eda0119
Michelle Liu <mliuu3259@gmail.com>
2023-04-12 16:24:46 -0400

    CI/CD (#25)
    * fixed frontend linting errors
    
    * created landing page
    
    * config settings in root
    
    * made landing page the root
    
    * login page
    
    * create and accept co-op pages
    
    * add PR template
    
    * linting
    
    * set up mocha
    
    * testing github action
    
    * frontend deployment via github pages
    
    * deploy frontend github action
    
    * deploy frontend github action
    
    * update delpoy action description
    
    * temp disable deploy action
    
    * re-enable deploy
    
    * delete deploy
    
    * add back deploy action
    
    * update deploy action
    
    * fixed deploy workflow syntax
    
    * change to cd walker
    
    * update build directory
    
    * update working-directory
    
    * delete npm run build step
    
    * added back npm run build
    
    * moved build to deploy
    
    * update publish_dir
    
    * make walker/build again
    
    * fly.io backend deployment
    
    * fly deploy github action backend
    
    * set working directory in backend deploy action
    
    * dont deploy until merged to master
    
    * change back to ts w express

---
+++ walker-backend/test/index.test.js
@@ -0,0 +1,10 @@
+var assert = require("assert");
+
+// Sample test; needs to be replaced with actual tests
+describe("Array", function () {
+    describe("#indexOf()", function () {
+        it("should return -1 when the value is not present", function () {
+            assert.equal([1, 2, 3].indexOf(4), -1);
+        });
+    });
+});
