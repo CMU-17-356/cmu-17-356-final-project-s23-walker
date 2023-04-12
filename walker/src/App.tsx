7d4c124deb43b07966cb573c8b232480ee8c4810
Michelle <mliuu3259@gmail.com>
2023-04-12 09:28:32 -0400

    deploy frontend github action

--- walker/src/App.tsx
+++ walker/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Create from "./pages/Create";
@@ -6,14 +6,14 @@ import Accept from "./pages/Accept";
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Landing />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/accept-invite" element={<Accept />} />
                 <Route path="/create-co-op" element={<Create />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
