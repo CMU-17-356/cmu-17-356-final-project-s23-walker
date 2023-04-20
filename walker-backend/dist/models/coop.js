c874a16b5b6084573588781d6aece740783126a9
Philip Kaufholz <pkaufhol@andrew.cmu.edu>
2023-04-19 21:15:53 -0400

    removed dist

--- walker-backend/dist/models/coop.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-var schema = new Schema({
-    users: {
-        type: [String],
-        default: []
-    },
-});
-var CoOp = model('CoOp', schema);
-export { CoOp };
