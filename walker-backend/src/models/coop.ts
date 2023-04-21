b8214870c4bf0315fb2f2e1a2e1ccf192228ee29
Michelle <mliuu3259@gmail.com>
2023-04-21 09:04:01 -0400

    update coop model to give the coop a name

--- walker-backend/src/models/coop.ts
+++ walker-backend/src/models/coop.ts
@@ -2,14 +2,17 @@ import { Schema, model } from 'mongoose';
 import { User, IUser } from './user.js';
 
 interface ICoOp {
-  users: IUser[]
+  users: IUser[],
+  name: string,
 }
 
 const coopSchema = new Schema({
   users: {
     type: [User.schema]
   },
-
+  name: {
+    type: String
+  }
 });
 
 const CoOp = model<ICoOp>('CoOp', coopSchema)
