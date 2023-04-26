55db60afedbc31a7dc7eac9be2a740aeaa703d6d
Michelle <mliuu3259@gmail.com>
2023-04-26 16:48:57 -0400

    add call to coop calls array

--- walker-backend/src/controllers/call.ts
+++ walker-backend/src/controllers/call.ts
@@ -1,4 +1,5 @@
-import { Call, ICall} from '../models/call.js';
+import { Call, ICall } from '../models/call.js';
+import { CoOp } from '../models/coop.js'
 import { User } from '../models/user.js'
 import { Request, Response } from 'express'
 
@@ -7,26 +8,30 @@ class CallController {
     const body = req.body
     const requester = await User.findById(body.requester) //check uniqueness
     body.requester = requester
+    const coop = await CoOp.findById(body.coop)
     const call = new Call(body)
     call.save()
       .then(() => {
-        res.status(200).json(`Call with requester ${requester?.person_name} and pet ${requester?.pet_name} created successfully.`);
+        coop?.calls.push(call)
+        coop?.save().then(() => {
+          return res.status(200).json(`Call with requester ${requester?.person_name} and pet ${requester?.pet_name} created successfully.`);
+        })
       })
       .catch((err: Error) => {
         return res.status(500).json(err)
       });
   };
-  
+
   public acceptCall = async (req: Request, res: Response) => {
-    const body = req.body    
+    const body = req.body
     const currAccepter = await User.findById(body.accepter)
     const currCall = await Call.findById(body.call)
-    const update = {accepter: currAccepter}
+    const update = { accepter: currAccepter }
     if (currCall) {
       currCall.updateOne(update)
-      .then((currCall: ICall | null) => {
-        return res.status(200).json(currCall)
-      })
+        .then((currCall: ICall | null) => {
+          return res.status(200).json(currCall)
+        })
     }
     else {
       return res.status(500).json(`Cannot find call with ID ${body.call}`)
